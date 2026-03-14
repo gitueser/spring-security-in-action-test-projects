72cfe14817fed3f3dc4c380a40a3eb0dd4591951
gitueser <jewellena78@gmail.com>
2026-03-14 18:39:41 +0100

    added rest point

--- ssia-ch2-ex1/src/main/java/com/laurentiuspilca/ssia/controllers/HelloController.java
+++ ssia-ch2-ex1/src/main/java/com/laurentiuspilca/ssia/controllers/HelloController.java
@@ -1,5 +1,13 @@
 package com.laurentiuspilca.ssia.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
 
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello!";
+    }
 }
