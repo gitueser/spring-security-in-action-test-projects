8ec8a3fe8223f0829124dbe63c3304f05ae223f9
gitueser <jewellena78@gmail.com>
2026-03-19 21:43:02 +0100

    played with implementations of StringKeyGenerator and BytesKeyGenerator

--- ssia-ch4/src/main/java/com/laurentiuspilca/ssia/security/Sha512PasswordEncoder.java
+++ ssia-ch4/src/main/java/com/laurentiuspilca/ssia/security/Sha512PasswordEncoder.java
@@ -1,11 +1,15 @@
 package com.laurentiuspilca.ssia.security;
 
+import org.springframework.security.crypto.keygen.BytesKeyGenerator;
+import org.springframework.security.crypto.keygen.KeyGenerators;
+import org.springframework.security.crypto.keygen.StringKeyGenerator;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 
 @Component
 public class Sha512PasswordEncoder implements PasswordEncoder {
@@ -31,6 +35,21 @@ public class Sha512PasswordEncoder implements PasswordEncoder {
 
     @Override
     public boolean matches(CharSequence rawPassword, String encodedPassword) {
+
+        StringKeyGenerator keyGeneratorStringKeyGenerator = KeyGenerators.string();
+        String salt = keyGeneratorStringKeyGenerator.generateKey();
+        System.out.println("MY StringKeyGenerator's salt is: " + salt);
+
+//        BytesKeyGenerator keyGeneratorBytesKeyGenerator = KeyGenerators.secureRandom(16);
+        BytesKeyGenerator keyGeneratorBytesKeyGenerator = KeyGenerators.shared(16);
+        byte [] key_01 = keyGeneratorBytesKeyGenerator.generateKey();
+        byte [] key_02 = keyGeneratorBytesKeyGenerator.generateKey();
+        int keyLength = keyGeneratorBytesKeyGenerator.getKeyLength();
+        System.out.println("MY BytesKeyGenerator's key_01 is: " + Arrays.toString(key_01));
+        System.out.println("MY BytesKeyGenerator's key_02 is: " + Arrays.toString(key_02));
+        System.out.println("MY BytesKeyGenerator's key length is: " + keyLength);
+
+
         String hashedRawPassword = encode(rawPassword);
         return hashedRawPassword.equals(encodedPassword);
     }
