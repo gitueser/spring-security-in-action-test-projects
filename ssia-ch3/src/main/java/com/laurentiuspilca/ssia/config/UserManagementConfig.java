ab80b101880097622c52dcab2fecc522998b5650
gitueser <jewellena78@gmail.com>
2026-03-19 16:56:40 +0100

    added example of working with UserDetails with LDAP (Lightweight Directory Access Protocol - hierarchical storage (like a tree)) implementation and LDIF = LDAP Data Interchange Format (it is simply a text format for describing LDAP data)

--- ssia-ch3/src/main/java/com/laurentiuspilca/ssia/config/UserManagementConfig.java
+++ ssia-ch3/src/main/java/com/laurentiuspilca/ssia/config/UserManagementConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.ldap.DefaultLdapUsernameToDnMapper;
+import org.springframework.security.ldap.DefaultSpringSecurityContextSource;
+import org.springframework.security.ldap.userdetails.LdapUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
 import javax.sql.DataSource;
@@ -13,19 +16,32 @@ import javax.sql.DataSource;
 public class UserManagementConfig {
 
     @Bean
-    public UserDetailsService userDetailsService(DataSource dataSource) {
-        String usersByUsernameQuery =
-                "select username, password, enabled from my_spring_schema.users where username = ?";
-        String authsByUserQuery =
-                "select username, authority from my_spring_schema.authorities where username = ?";
+    public UserDetailsService userDetailsService() {
+        var cs = new DefaultSpringSecurityContextSource("ldap://127.0.0.1:33389/dc=springframework,dc=org");
+        cs.afterPropertiesSet();
 
-        var userDetailsManager = new JdbcUserDetailsManager(dataSource);
-        userDetailsManager.setUsersByUsernameQuery(usersByUsernameQuery);
-        userDetailsManager.setAuthoritiesByUsernameQuery(authsByUserQuery);
-        return userDetailsManager;
-//        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
-//        return jdbcUserDetailsManager;
+        var manager = new LdapUserDetailsManager(cs);
+        manager.setUsernameMapper(new DefaultLdapUsernameToDnMapper("ou=groups", "uid"));
+        manager.setGroupSearchBase("ou=groups");
+
+        return manager;
     }
+
+//    @Bean
+//    public UserDetailsService userDetailsService(DataSource dataSource) {
+//        String usersByUsernameQuery =
+//                "select username, password, enabled from my_spring_schema.users where username = ?";
+//        String authsByUserQuery =
+//                "select username, authority from my_spring_schema.authorities where username = ?";
+//
+//        var userDetailsManager = new JdbcUserDetailsManager(dataSource);
+//        userDetailsManager.setUsersByUsernameQuery(usersByUsernameQuery);
+//        userDetailsManager.setAuthoritiesByUsernameQuery(authsByUserQuery);
+//        return userDetailsManager;
+
+    /// /        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+    /// /        return jdbcUserDetailsManager;
+//    }
 
 
 //    @Bean
@@ -49,7 +65,6 @@ public class UserManagementConfig {
 //        userDetailsService.createUser(user);
 //        return userDetailsService;
 //    }
-
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
