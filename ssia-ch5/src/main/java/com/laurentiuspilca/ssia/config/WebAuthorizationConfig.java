2caf97daf721e3d5a327826dea2820b65d390407
gitueser <jewellena78@gmail.com>
2026-03-20 17:58:20 +0100

    added new custom filter StaticKeyAuthenticationFilter to test addFilterAt() method. To check with curl use this:  curl -i -u john:12345 -H "Authorization:SD9cICjl1e" http://localhost:8080/hello; echo

--- ssia-ch5/src/main/java/com/laurentiuspilca/ssia/config/WebAuthorizationConfig.java
+++ ssia-ch5/src/main/java/com/laurentiuspilca/ssia/config/WebAuthorizationConfig.java
@@ -2,17 +2,21 @@ package com.laurentiuspilca.ssia.config;
 
 //import com.laurentiuspilca.ssia.security.CustomAuthenticationProvider;
 
-import com.laurentiuspilca.ssia.security.AuthenticationLoggingFilter;
-import com.laurentiuspilca.ssia.security.RequestValidationFilter;
+import com.laurentiuspilca.ssia.security.StaticKeyAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 public class WebAuthorizationConfig {
+
+    private final StaticKeyAuthenticationFilter staticKeyAuthenticationFilter;
+
+    public WebAuthorizationConfig(StaticKeyAuthenticationFilter staticKeyAuthenticationFilter) {
+        this.staticKeyAuthenticationFilter = staticKeyAuthenticationFilter;
+    }
 
 //    private final CustomAuthenticationProvider authenticationProvider;
 //
@@ -25,15 +29,17 @@ public class WebAuthorizationConfig {
 
         http
 //                .addFilterBefore(new MyFilter(), BasicAuthenticationFilter.class)
-                .addFilterBefore(new RequestValidationFilter(), BasicAuthenticationFilter.class)
-                .addFilterAfter(new AuthenticationLoggingFilter(), BasicAuthenticationFilter.class)
-                .csrf(csrf -> csrf.ignoringRequestMatchers("/h2-console/**"))
-                .headers(headers -> headers.frameOptions(frame -> frame.sameOrigin()))
-                .authorizeHttpRequests(c -> c
-                        .requestMatchers("/h2-console/**").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .httpBasic(Customizer.withDefaults());
+//                .addFilterBefore(new RequestValidationFilter(), BasicAuthenticationFilter.class)
+                .addFilterAt(staticKeyAuthenticationFilter, BasicAuthenticationFilter.class)
+                .authorizeHttpRequests(c -> c.anyRequest().permitAll());
+//                .addFilterAfter(new AuthenticationLoggingFilter(), BasicAuthenticationFilter.class)
+//                .csrf(csrf -> csrf.ignoringRequestMatchers("/h2-console/**"))
+//                .headers(headers -> headers.frameOptions(frame -> frame.sameOrigin()))
+//                .authorizeHttpRequests(c -> c
+//                        .requestMatchers("/h2-console/**").permitAll()
+//                        .anyRequest().authenticated()
+//                );
+//                .httpBasic(Customizer.withDefaults());
 
         return http.build();
     }
