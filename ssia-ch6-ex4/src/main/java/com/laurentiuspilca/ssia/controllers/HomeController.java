5ac9b27e4e4b0f169d4e8943669957e7f7b9a695
gitueser <jewellena78@gmail.com>
2026-03-26 19:22:53 +0100

    added home page and appropriate controller
    Note that the home page is not a @RestController, but a simple @Controller. Because of this, Spring doesn't send the method's return value in the HTTP response. Instead, it finds and displays a view named home.html.

---
+++ ssia-ch6-ex4/src/main/java/com/laurentiuspilca/ssia/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.laurentiuspilca.ssia.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/home")
+    public String home() {
+        return "home.html";
+    }
+}
