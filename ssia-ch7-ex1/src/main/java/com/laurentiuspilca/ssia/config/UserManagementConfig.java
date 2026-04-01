fae572ca42c401223bd74c8c3556335f02ccb4a1
gitueser <jewellena78@gmail.com>
2026-04-01 20:12:57 +0200

    changed SecurityFilterChain to pass users with "ADMIN" role but with help of roles() method instead of authorities() method. Also note that when using the roles() method, you do not need to use the prefix ROLE_, as was done when using the authorities() method.

--- ssia-ch7-ex1/src/main/java/com/laurentiuspilca/ssia/config/UserManagementConfig.java
+++ ssia-ch7-ex1/src/main/java/com/laurentiuspilca/ssia/config/UserManagementConfig.java
@@ -17,13 +17,13 @@ public class UserManagementConfig {
         uds.createUser(
                 User.withUsername("john")
                         .password("12345")
-                        .authorities("ROLE_ADMIN")
+                        .roles("ADMIN")
                         .build()
         );
         uds.createUser(
                 User.withUsername("jane")
                         .password("12345")
-                        .authorities("ROLE_MANAGER")
+                        .roles("MANAGER")
                         .build()
         );
         return uds;
