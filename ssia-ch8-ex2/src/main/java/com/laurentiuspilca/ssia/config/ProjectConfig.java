b3b8cdae831d5f1bd20041eeb3f390e37b0fc692
gitueser <jewellena78@gmail.com>
2026-04-03 18:32:49 +0200

    created 'ssia-ch8-ex2' project based on 'ssia-ch8-ex1' project

---
+++ ssia-ch8-ex2/src/main/java/com/laurentiuspilca/ssia/config/ProjectConfig.java
@@ -0,0 +1,25 @@
+package com.laurentiuspilca.ssia.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
+
+@Configuration
+public class ProjectConfig {
+
+    @Bean
+    SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http.httpBasic(Customizer.withDefaults());
+
+        http.authorizeHttpRequests(c -> c
+                .requestMatchers("/hello").hasRole("ADMIN")
+                .requestMatchers("/ciao").hasRole("MANAGER")
+                .anyRequest().permitAll()
+        );
+
+        return http.build();
+    }
+}
---
+++ ssia-ch8-ex2/src/main/java/com/laurentiuspilca/ssia/config/UserManagementConfig.java
@@ -0,0 +1,36 @@
+package com.laurentiuspilca.ssia.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class UserManagementConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        var uds = new InMemoryUserDetailsManager();
+        uds.createUser(
+                User.withUsername("john")
+                        .password("12345")
+                        .roles("ADMIN")
+                        .build()
+        );
+        uds.createUser(
+                User.withUsername("jane")
+                        .password("12345")
+                        .roles("MANAGER")
+                        .build()
+        );
+        return uds;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
