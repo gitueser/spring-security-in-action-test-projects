e199667dce04c6730bc36b16e7d4424734b41a48
gitueser <jewellena78@gmail.com>
2026-04-29 20:21:46 +0200

    Added CORS configuration to 'ProjectConfig' to apply common rules across the whole application

--- ssia-ch10-ex1/src/main/java/com/laurentiuspilca/ssia/config/ProjectConfig.java
+++ ssia-ch10-ex1/src/main/java/com/laurentiuspilca/ssia/config/ProjectConfig.java
@@ -4,6 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 public class
@@ -11,6 +15,17 @@ ProjectConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.cors(corsConfigurer -> {
+            CorsConfigurationSource source = request ->  {
+                CorsConfiguration config = new CorsConfiguration();
+                config.setAllowedOrigins(List.of("http://localhost:8080", "example.com", "example.org"));
+                config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+                config.setAllowedHeaders(List.of("*"));
+                return config;
+            };
+            corsConfigurer.configurationSource(source);
+        });
+
         http.csrf(c -> c.disable());
         http.authorizeHttpRequests(
                 c -> c.anyRequest().permitAll()
