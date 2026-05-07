b52b03dfecea5636f6564290a1c1da4a619458f6
gitueser <jewellena78@gmail.com>
2026-05-07 17:45:43 +0200

    added an example of @PostFilter annotation work (It's also important to remember that both the @PreFilter and @PostFilter annotations only work with collections or arrays, and only with mutable ones. If the collection or array is immutable, we'll get a 500 error: java.lang.UnsupportedOperationException: null at java.base/java.util.ImmutableCollections.uoe(ImmutableCollections.                                                  java:73) ~[na:na]). To check: 1) curl -i -u julien:12345 http://localhost:8080/find
    2) curl -i -u nikolai:12345 http://localhost:8080/find

--- ssia-ch12-ex1/src/main/java/com/laurentiuspilca/ssia/controllers/ProductController.java
+++ ssia-ch12-ex1/src/main/java/com/laurentiuspilca/ssia/controllers/ProductController.java
@@ -26,4 +26,9 @@ public class ProductController {
 
         return productService.sellProducts(products);
     }
+
+    @GetMapping("/find")
+    public List<Product> findProducts() {
+        return productService.findProducts();
+    }
 }
